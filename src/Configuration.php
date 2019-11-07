3032c18554157e0427425e693269be6a5cae5e42
Andrey Vorobyev <vorobivka@mail.ru>
2019-11-07 10:23:40 +0000

    Fix pipelines

--- src/Configuration.php
+++ src/Configuration.php
@@ -397,7 +397,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    OpenAPI Spec Version: 2' . PHP_EOL;
-        $report .= '    SDK Package Version: 2.0.865' . PHP_EOL;
+        $report .= '    SDK Package Version: 2.0.929' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
